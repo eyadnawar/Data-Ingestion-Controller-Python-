c9d6dd75ea5e6f55f7f203ffaa9a1cdea5d86a29
enawar <efarouknawar@gmail.com>
2021-05-14 01:13:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,79 @@
+#Description
+This system is a ***data ingestor controller*** which is a sub-system needed to serve as a reliable intermediary between the user-facing 
+applications and the dataset ingestors.
+
+There are many data ingestors and and ingestors are not 
+equal since each is skilled differently in dealing with one or more data source protocols.
+
+In Simple words, a data ingestor controller is like a **demultiplexer**. 
+A demultiplexer (or demux) is a device that takes a **single input line** *(in this case, an ingestion job with a specific data source protocol)* and ***routes*** it to **one of several digital output lines** *(in this case, the appropriate data ingestors)*
+
+This system which acts as a Network Service for communication through a REST API performs 5 operations:
+* Accept a DatasetIngestionJob and store it. The job has a unique ID, a user ID, an urgency boolean flag, a 
+data-source protocol (jdbc|odbc|s3|looker), and a text query in a language accepted by the dataset source.
+  
+* Respond to a request from a dataset ingestor asking for a suitable job. The jobs handed out should be 
+prioritized by their creation order, unless if they’re urgent, in which case urgent jobs have higher priority 
+than non-urgent ones. Requests from ingestors should contain their advertised protocol capabilities, so 
+ingestors should only be given jobs that they can handle.
+  
+* Accept a DatasetIngestionResult and store it. The result contains a unique ID, the job ID, the status 
+of the execution of the job (success|failure), and the produced dataset.
+  
+* Respond to a request from a user-facing application asking for the ingestion status of a job it had 
+provided earlier. It should produce a response with the ingestion status of either (successful|pending|failed).
+  
+* Respond to a request from an insight generation system asking for the dataset produced for a job, given 
+its ID.
+
+##Building, Testing, Running, and Connecting to the Service
+
+This system is implemented in Python's Flask. To build, just clone the repo
+by running the following command in Git Bash in the appropriate directory:
+
+``git clone <link to repo>``
+
+In your favorite IDE, you can open the terminal and run `flask run` or just open the `main.py` file, right click and choose build and run. It will run on the localhost.
+
+To conect to the service, there are 5 endpoints that correspond to each of the aforementioned operations. These endpoints are:
+
+* `/submit-job` which is a `PUT` method sent to the controller by the user-facing application. The controller receives the following parameters in the body of the request:
+
+    1. `user_id`: String  
+    2. `urgency`: Boolean
+    3. `data_source_protocol`: String
+    4. `text_query`: String
+    5. `url`: String 
+       
+        and returns a response with the Job ID created
+
+
+* `/ingestor-request-job` which is a `GET` method sent by the controller to the requesting data ingestor. The controller recieves the `data_source_protocol` as parameter in the body of the GET request, and returns the following in the response:
+
+    1. `job_id`: String
+    2. `text_query`: String  {The query to be executed to retrieve the dataset}
+    3. `url`: String   {The url where the query will be executed to retrieve the dataset}
+
+
+* `/post-ingestion-result` which is a `POST` method that is sent to the controller by a data ingestor to inform the former about the data ingestion result of a specific job, and the body of the request contains the following parameters:
+
+    1. `job_id`: String
+    2. `ingestion_status`: String       {Success/failure}
+    3. `dataset_location`: String       {uri for the dataset location on the disk (metadata)}
+
+
+* `/enquire-about-job` which is a `GET` method sent to the controller by the user-facing application. The controller receives the `job_id` as a parameter in the body of the request, searches the database for the job with the matching `job_id`, and checks the `ingestion_status` of that job. The controller responds with `Success/Failure/Pending`
+
+
+*   `/request_dataset` which is a `GET` method sent to the controller by the insight generation system asking for the location of a dataset given the id of that job. The controller receives `job_id` in the body of the request and responds with the `dataset_location`
+
+
+## Technical *(Implementation)* Details
+
+This system is built in Python's `Flask`, and uses `MongoDB` as its database storage system.
+
+The state of the data ingestor controller is stroed in the app's main memory. The state of the controller is represented by 2 queues *(urgent/non-urgent)* for each of the data source protocols that the controller supports `(jdbc|odbc|s3|looker)`
+The controller delegates jobs to the appropriate data ingestors from the non-urgent queue only if the urgent queue is empty.
+
+The 2 queues approach was better *efficiency-wise* than using 1 `queue` or 1 `Arraylist`. The reason is that an `ArrayList`
+ would take `O(n)` for `insertion` and `O(n)` for `deletion`, whereas inserting and deleting from the queue is a cheap `O(1)` operation.
---
+++ main.py
@@ -0,0 +1,315 @@
+'''# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/'''
+from queue import Queue
+import json
+from flask import Flask, request, jsonify, make_response, render_template, session
+import uuid
+from flask_cors import CORS
+from flask_mongoengine import MongoEngine
+from mongoengine.queryset.visitor import Q
+
+app = Flask(__name__)
+CORS(app)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'kausa_task',
+    'host': 'mongodb+srv://eyad-admin:select33@cluster0.gutxm.mongodb.net/kausa_task',
+    #'port': 27017
+}
+db = MongoEngine()
+db.init_app(app)
+
+class Job(db.Document):
+    job_id = db.StringField()  ## unique
+    user_id = db.StringField()
+    urgency = db.BooleanField()
+    data_source_protocol = db.StringField()
+    text_query = db.StringField()
+    url = db.StringField()
+    result_id = db.StringField()   ## unique
+    ingestion_status = db.StringField()
+    dataset_location = db.StringField()
+    def to_json(self):
+        return {"job_id": self.job_id,
+                "user_id": self.user_id,
+                "urgency": self.urgency,
+                "data_source_protocol": self.data_source_protocol,
+                "text_query": self.text_query,
+                "url": self.url,
+                "result_id": self.result_id,
+                "ingestion_status": self.ingestion_status,
+                "dataset_location": self.dataset_location}
+
+
+
+@app.route('/submit-job', methods=['PUT'])
+def submit_job():
+    ## Get request body params (from request.data)
+    record = json.loads(request.data)
+    job_id = str(uuid.uuid1().int)
+    user_id = record['user_id']
+    urgency = record['urgency']
+    data_source_protocol = record['data_source_protocol']
+    text_query = record['text_query']
+    url = record['url']
+    #print(urgency)
+    """result_id = record['result_id']
+    ingestion_status = record['ingestion_status']
+    dataset_location = record['dataset_location']"""
+    #user = User.objects(Q(username=username) | Q(email=email)).first()
+    ## Make sure no user registered with same email & username
+    if data_source_protocol.lower() not in ['jdbc', 'odbc', 's3', 'looker']:
+        return make_response(
+            'This protocol is not yet supported, please choose of either [jdbc, odbc, s3, looker], and try again',
+            400,
+        )
+    if (data_source_protocol.lower() == 'jdbc'):
+        if (urgency == True):
+            global urgent_jdbc
+            urgent_jdbc.put({
+                'job_id': job_id,
+                'text_query': record['text_query'],
+                'url': record['url']
+            })
+        else:
+            global jdbc
+            jdbc.put({
+                'job_id': job_id,
+                'text_query': record['text_query'],
+                'url': record['url']
+            })
+    elif (data_source_protocol.lower() == 'odbc'):
+        if (urgency == True):
+            global urgent_odbc
+            urgent_odbc.put({
+                'job_id': job_id,
+                'text_query': record['text_query'],
+                'url': record['url']
+            })
+        else:
+            global odbc
+            odbc.put({
+                'job_id': job_id,
+                'text_query': record['text_query'],
+                'url': record['url']
+            })
+    elif (data_source_protocol.lower() == 's3'):
+        if (urgency == True):
+            global urgent_s3
+            urgent_s3.put({
+                'job_id': job_id,
+                'text_query': record['text_query'],
+                'url': record['url']
+            })
+        else:
+            global s3
+            s3.put({
+                'job_id': job_id,
+                'text_query': record['text_query'],
+                'url': record['url']
+            })
+    else:
+        if (urgency == True):
+            global urgent_looker
+            urgent_looker.put({
+                'job_id': job_id,
+                'text_query': record['text_query'],
+                'url': record['url']
+            })
+        else:
+            global looker
+            looker.put({
+                'job_id': job_id,
+                'text_query': record['text_query'],
+                'url': record['url']
+            })
+
+
+    job_to_save = Job(job_id = job_id,
+                     user_id = record['user_id'],
+                     urgency = record['urgency'],
+                     data_source_protocol = record['data_source_protocol'],
+                     text_query = record['text_query'],
+                     url = record['url'],
+                     result_id= "",
+                     ingestion_status = "",
+                     dataset_location = ""
+    )
+    job_to_save.save()
+    ## Return success message with status code 200
+    return make_response(
+    {'message': 'Job successfully Registered!', 'Job ID': job_id,},
+    200,
+    )
+
+
+
+@app.route('/ingestor-request-job', methods=['GET'])
+def request_job():
+    ## Get request body params (from request.data)
+    record = json.loads(request.data)
+    data_source_protocol = record['data_source_protocol']
+    ## retrieve job from relevant queue
+    if(data_source_protocol.lower() == 'jdbc'):
+        print('test here')
+        if(urgent_jdbc.empty()):
+            if(jdbc.empty()):
+                return make_response(
+                    'No jdbc type jobs available for ingestion',
+                    400
+                )
+            else:
+                return make_response(
+                    {'job': jdbc.get_nowait()},
+                    200
+                )
+        else:
+            return make_response(
+                {'job': urgent_jdbc.get_nowait()},
+                200
+            )
+    elif (data_source_protocol.lower() == 'odbc'):
+        if (urgent_odbc.empty()):
+            if (odbc.empty()):
+                return make_response(
+                    'No odbc type jobs available for ingestion',
+                    400
+                )
+            else:
+                return make_response(
+                    {'job': odbc.get_nowait()},
+                    200
+                )
+        else:
+            return make_response(
+                {'job': urgent_odbc.get_nowait()},
+                200
+            )
+    elif (data_source_protocol.lower() == 's3'):
+        if (urgent_s3.empty()):
+            if (s3.empty()):
+                return make_response(
+                    'No s3 type jobs available for ingestion',
+                    400
+                )
+            else:
+                return make_response(
+                    {'job': s3.get_nowait()},
+                    200
+                )
+        else:
+            return make_response(
+                {'job': urgent_s3.get_nowait()},
+                200
+            )
+    else:
+        if (urgent_looker.empty()):
+            if (looker.empty()):
+                return make_response(
+                    'No looker type jobs available for ingestion',
+                    400
+                )
+            else:
+                return make_response(
+                    {'job': looker.get_nowait()},
+                    200
+                )
+        else:
+            return make_response(
+                {'job': urgent_looker.get_nowait()},
+                200
+            )
+
+@app.route('/post-ingestion-result', methods=['POST'])
+def update_data_ingestion_result():
+    record = json.loads(request.data)
+    job_id = record['job_id']
+    result_id = str(uuid.uuid1().int)
+    ingestion_status = record['ingestion_status']
+    dataset_location = record['dataset_location']
+    job = Job.objects(job_id= job_id).first()
+    job.result_id = result_id
+    job.ingestion_status = ingestion_status
+    job.dataset_location = dataset_location
+    job.save()
+    return make_response(
+        'job status updated successfully!',
+        200
+    )
+
+@app.route('/enquire-about-job', methods=['GET'])
+def enquire_about_job():
+    record = json.loads(request.data)
+    job_id = record['job_id']
+    job = Job.objects(job_id = job_id).first()
+    if (not job):
+        return make_response(
+            'There is no job with the specified ID',
+            400
+        )
+    ingestion_status = job.ingestion_status
+    if(ingestion_status != ""):
+        return make_response(
+            "The status of the job with ID %s is: %s" %(job_id, ingestion_status),
+            200
+        )
+    else:
+        return make_response(
+            "The status of the job with ID %s is: still pending" %(job_id),
+            200
+        )
+
+@app.route('/request_dataset', methods=['GET'])
+def request_dataset():
+    record = json.loads(request.data)
+    job_id = record['job_id']
+    job = Job.objects(job_id = job_id).first()
+    if(not job):
+        return make_response(
+            'There is no job with the specified ID',
+            400
+        )
+    dataset_location = job.dataset_location
+    if(dataset_location != ""):
+        return make_response(
+            "The location of the dataset for the job with ID %s is: %s" %(job_id, dataset_location),
+            200
+        )
+    else:
+        return make_response(
+            "The location of the dataset for the job with ID %s is unavailable at the moment, as the ingestion of that job is still pending" %(job_id),
+            200
+        )
+
+if __name__ == "__main__":
+    global jdbc
+    global urgent_jdbc
+    global odbc
+    global urgent_odbc
+    global s3
+    global urgent_s3
+    global looker
+    global urgent_looker
+
+    jdbc = Queue(maxsize=10)
+    urgent_jdbc = Queue(maxsize=10)
+    odbc = Queue(maxsize=10)
+    urgent_odbc = Queue(maxsize=10)
+    s3 = Queue(maxsize=10)
+    urgent_s3 = Queue(maxsize=10)
+    looker = Queue(maxsize=10)
+    urgent_looker = Queue(maxsize=10)
+
+    app.run(debug=True)
